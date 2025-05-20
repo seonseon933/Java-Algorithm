da2fbfb0ef3e1d69191879824e11b9067c657f22
seonhyelin <seonseon933@gmail.com>
2025-05-20 21:38:43 +0900

    <8주차> BFS - 103, 1302

---
+++ src/Binary_Trees/BFS/P103_Binary_Tree_Zigzag_Level_Order_Traversal/P103_Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -0,0 +1,53 @@
+package Binary_Trees.BFS.P103_Binary_Tree_Zigzag_Level_Order_Traversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class P103_Binary_Tree_Zigzag_Level_Order_Traversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> bfs = new LinkedList<>();
+        bfs.offer(root);
+
+        boolean leftToRight = true;
+
+        while (!bfs.isEmpty()) {
+            int size = bfs.size();
+            LinkedList<Integer> level = new LinkedList<>();
+
+            for(int i = 0; i < size; i++){
+                TreeNode node = bfs.poll();
+
+                if(leftToRight){
+                    level.addLast(node.val);
+                }else{
+                    level.addFirst(node.val);
+                }
+
+                if (node.left != null) bfs.offer(node.left);
+                if (node.right != null) bfs.offer(node.right);
+            }
+
+            res.add(level);
+            leftToRight = !leftToRight;
+        }
+        return res;
+    }   
+}
---
+++ src/Binary_Trees/BFS/p1302_Deepest_Leaves_Sum/p1302_Deepest_Leaves_Sum.java
@@ -0,0 +1,43 @@
+package Binary_Trees.BFS.p1302_Deepest_Leaves_Sum;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class p1302_Deepest_Leaves_Sum {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int deepestLeavesSum(TreeNode root) {
+        if(root == null) return 0;
+
+        Queue<TreeNode> bfs = new LinkedList<>();
+        int res = 0;
+
+        bfs.offer(root);
+        
+        while (!bfs.isEmpty()) {
+            int size = bfs.size();
+            res = 0;
+
+            for(int i = 0; i < size; i++){
+                TreeNode node = bfs.poll();
+                res += node.val;
+
+                if(node.left != null) bfs.offer(node.left);
+                if(node.right != null) bfs.offer(node.right);
+            }
+        }        
+
+        return res;
+    }
+}
