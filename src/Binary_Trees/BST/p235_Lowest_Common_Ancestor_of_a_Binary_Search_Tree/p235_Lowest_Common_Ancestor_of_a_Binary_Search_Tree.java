99ee7253fe1b56819e517f2736680dedeac5b17d
seonhyelin <seonseon933@gmail.com>
2025-05-27 22:28:43 +0900

    <9주차> BST문제 - 2문제제

---
+++ src/Binary_Trees/BST/p235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/p235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,22 @@
+package Binary_Trees.BST.p235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
+
+public class p235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        else if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        else return root;
+    }
+}
